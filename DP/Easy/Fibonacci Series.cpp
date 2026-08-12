03f510d664419218e0bb0c539bcc03e6eeae88fd
saiprasanth201 <spk782005@gmail.com>
2026-08-12 12:38:17 +0530

    Day 74 - Fibonacci Series using Memoization,Tabulation,Space Optimization DP Solution.

--- DP/Easy/Fibonacci Series.cpp
+++ DP/Easy/Fibonacci Series.cpp
@@ -13,3 +13,33 @@ public:
         return solveDP(n,dp);
     }
 };
+
+//Tabulation - Bottom Up DP.
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1)return n;
+        vector<int>dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+//Space Optimization solution.
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+        int prev2 = 0, prev = 1, curr;
+        for(int i = 2;i<=n;i++){
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};

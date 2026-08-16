a460b382c5fa295b77a20b11d037225b15daeb03
saiprasanth201 <spk782005@gmail.com>
2026-08-16 22:40:58 +0530

    Day 78 - BFS Traversal of Graphs.

---
+++ Graphs/Easy/BFS_Traversal.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+    vector<int>bfs(vector<vector<int>>adj){
+        int v = adj.size();
+        vector<int>vis(v,0);
+        vis[0] = 1;
+        queue<int>q;
+        q.push(0);
+        vector<int>bfs;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push(node);
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+}

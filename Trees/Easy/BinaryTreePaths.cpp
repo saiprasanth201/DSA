33e140def319c56ee288e59396022fc26fe4e785
saiprasanth201 <spk782005@gmail.com>
2026-09-01 22:44:09 +0530

    Day 94 - Binary Tree Paths Solution.

---
+++ Trees/Easy/BinaryTreePaths.cpp
@@ -0,0 +1,22 @@
+class Solution{
+public:
+    void allpaths(TreeNode* root, string path, vector<string>&ans){
+        if(root->left == NULL && root->right == NULL){
+            ans.push_back(path);
+            return;
+        }
+        if(root->left){
+            allpaths( root->left,path + "->" + to_string(root->left->val),ans);
+        }
+        if(root->right){
+            allpaths( root->right,path + "->" + to_string(root->right->val),ans);
+        }
+    }
+    vector<string>binaryTreePaths(TreeNode* root){
+        string path = to_string(root->val);
+        vector<string>ans;
+        if (root == nullptr) return ans;
+        allpaths(root,path,ans);
+        return ans;
+    }
+};

ccb886f2211dcee60815b0197951919d62097a4f
saiprasanth201 <spk782005@gmail.com>
2026-09-01 21:27:03 +0530

    Day 94 - Zig Zag Level Order Traversal of Binary Tree Solution.

---
+++ Trees/Medium/ZigZagLevelOrderTraversal.cpp
@@ -0,0 +1,25 @@
+class Solution{
+public:
+    vector<vector<int>>zigzagLevelOrder(TreeNode* root){
+        vector<vector<int>>ans;
+        if(root == nullptr) return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        bool LtoR = true;
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>level;
+            for(int i =0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
+            }
+            if(!LtoR) reverse(level.begin(),level.end());
+            ans.push_back(level);
+            LtoR = !LtoR;
+        }
+        return ans;
+    }
+};

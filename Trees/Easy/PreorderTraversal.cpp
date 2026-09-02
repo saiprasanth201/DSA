a6d5118ff3d318905d235dc58049254269afe78b
saiprasanth201 <spk782005@gmail.com>
2026-09-02 09:06:56 +0530

    Day 95 - Iterative Preorder Traversal of a BST Solution.

--- Trees/Easy/PreorderTraversal.cpp
+++ Trees/Easy/PreorderTraversal.cpp
@@ -1,3 +1,4 @@
+//Recursion
 class Solution{
     public:
     void preorder(TreeNode*root, vector<int>&arr){
@@ -10,5 +11,28 @@ class Solution{
         vector<int>arr;
         preorder(root,arr);
         return arr;
+    }
+};
+
+//Iterative
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>preorder;
+        if(root == nullptr) return preorder;
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right != nullptr){
+                st.push(root->right);
+            }
+            if(root->left != nullptr){
+                st.push(root->left);
+            }
+        }
+        return preorder;
     }
 };

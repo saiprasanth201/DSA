5e0f437ea8e2085fdd729e7f122f0fc9cc51f8e7
saiprasanth201 <spk782005@gmail.com>
2026-09-05 17:38:28 +0530

    Day 98 - Sqrt of X using Binary Search Solution.

---
+++ Binary Search/BS on Arrays/Sqrt(x).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long left = 0, right = x;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long sq = mid*mid;
+            if(sq == x) return mid;
+            if (sq < x) left = mid + 1;
+            else if (sq > x)right = mid - 1; 
+            } 
+        return right;
+    }
+};

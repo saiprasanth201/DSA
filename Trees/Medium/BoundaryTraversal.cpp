a6b76914264aaac4eee2def1194d713f2dd991c1
saiprasanth201 <spk782005@gmail.com>
2026-09-07 19:12:55 +0530

    Day 100 - Boundary Traversal of a Binary Tree Solution

---
+++ Trees/Medium/BoundaryTraversal.cpp
@@ -0,0 +1,43 @@
+class Solution{
+public:
+    bool isLeaf(TreeNode* node){
+        return(!node->left && !node->right);
+    }
+    void addleftB(TreeNode* root, vector<int>&ans){
+        TreeNode* curr = root->left;
+        while(curr){
+            if(!isLeaf(curr)) ans.push_back(curr->val);
+            if(curr->left) curr = curr->left;
+            else curr = curr->right;
+        }
+    }
+    void addrightB(TreeNode* root, vector<int>&ans){
+        TreeNode* curr = root->right;
+        vector<int>temp;
+        while(curr){
+            if(!isLeaf(curr))temp.push_back(curr->val);
+            if(curr->right) curr = curr->right;
+            else curr = curr->left;
+        }
+        for(int i = temp.size() - 1; i >= 0 ; i--){
+            ans.push_back(temp[i]);
+        }
+    }
+    void addleaf(TreeNode* root, vector<int>&ans){
+        if(isLeaf(root)){
+        ans.push_back(root->val);
+        return;
+    }
+    if(root->left) addleaf(root->left,ans);
+    if(root->right) addleaf(root->right,ans);
+}
+    vector<int>BoundaryTraversal(TreeNode* root){
+        vector<int>ans;
+        if(!root) return ans;
+        if(!isLeaf(root)) ans.push_back(root->val);
+        addleftB(root,ans);
+        addleaf(root,ans);
+        addrightB(root,ans);
+        return ans;
+    }
+};

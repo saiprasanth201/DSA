b8991eab06f4138ef0cc6907e6dffede4d0c26af
saiprasanth201 <spk782005@gmail.com>
2026-09-16 08:38:45 +0530

    Day 109 - Count Negative numbers in a sorted matrix Solution

--- Binary Search/BS on 2D Arrays/Count Negative Numbers in a Sorted Matrix.cpp
+++ Binary Search/BS on 2D Arrays/Count Negative Numbers in a Sorted Matrix.cpp
@@ -1,4 +1,4 @@
-//M1:Bruteforce.
+//M1:Bruteforce. O(n^2)
 class Solution {
     public:
         int countNegatives(vector<vector<int>>& grid) {
@@ -7,6 +7,25 @@ class Solution {
                 for(int c=0;c<grid[r].size();c++){
                     if(grid[r][c]<0)count++;
                 }
+            }
+            return count;
+        }
+    };
+
+//M2: Optimal. O(m+n)
+class Solution {
+    public:
+        int countNegatives(vector<vector<int>>& grid) {
+            int m = grid.size(),n = grid[0].size();
+            int i = m-1; // row
+            int j = 0;   // col
+            int count = 0;
+            while(i>=0 && j<n){
+                if(grid[i][j] < 0){
+                    count += n-j;
+                    i--;
+                }
+                else j++;
             }
             return count;
         }

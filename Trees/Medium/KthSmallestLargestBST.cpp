77445ad4baeaf36b8d1aaa700dbf56587dafb9e9
saiprasanth201 <spk782005@gmail.com>
2026-09-24 12:51:06 +0530

    Day 117 - Kth Smallest & Largest Elements of BST Solution

---
+++ Trees/Medium/KthSmallestLargestBST.cpp
@@ -0,0 +1,34 @@
+// LC Version:
+class Solution {
+    public:
+        void inorder(TreeNode*root,vector<int>&ans){
+            if(!root)return;
+            inorder(root->left,ans);
+            ans.push_back(root->val);
+            inorder(root->right,ans);
+        }
+        int kthSmallest(TreeNode* root, int k) {
+            vector<int>ans;
+            inorder(root,ans);
+            return ans[k-1];
+        }
+    };
+
+// TUF Version :
+class Solution {
+    public:
+        void inorder(TreeNode*root,vector<int>&ans){
+            if(!root)return;
+            inorder(root->left,ans);
+            ans.push_back(root->data);
+            inorder(root->right,ans);
+        }
+        vector<int> kLargesSmall(TreeNode* root, int k) {
+            vector<int>ans;
+            inorder(root,ans);
+            int n = ans.size();
+            return {ans[k-1],ans[n-k]};
+        }
+    };
+
+    Day 117 - Kth Smallest & Largest Elements of BST Solution

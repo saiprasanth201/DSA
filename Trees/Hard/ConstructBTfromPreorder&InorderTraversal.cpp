64e1bf0ee8edc523f1a4859627e450c1f726e785
saiprasanth201 <spk782005@gmail.com>
2026-09-24 19:46:13 +0530

    Day 117 - Construct BT From Preorder and Inorder Traversal of BST Optimal Solution

--- Trees/Hard/ConstructBTfromPreorder&InorderTraversal.cpp
+++ Trees/Hard/ConstructBTfromPreorder&InorderTraversal.cpp
@@ -1,3 +1,4 @@
+//Linear Search Version:
 class Solution {
     public:
         int search(vector<int>& inorder,int left,int right,int val){
@@ -17,5 +18,24 @@ class Solution {
         TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
            int preidx = 0;
            return helper(preorder,inorder,preidx,0,inorder.size()-1); 
+        }
+    };
+
+// Unordered map version
+class Solution {
+    public:
+        TreeNode* helper(vector<int>& preorder, vector<int>&inorder,unordered_map<int,int>&inmap,int & preidx, int left, int right){
+            if(left>right) return nullptr;
+            TreeNode* root = new TreeNode(preorder[preidx]);
+            int inidx = inmap[preorder[preidx]];preidx++;
+            root->left = helper(preorder,inorder,inmap,preidx,left,inidx-1);
+            root->right = helper(preorder,inorder,inmap,preidx,inidx+1,right);
+            return root;
+        }
+        TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+            unordered_map<int,int>inmap;
+            for(int i=0;i<inorder.size();i++)inmap[inorder[i]] = i;
+            int preidx = 0;
+            return helper(preorder,inorder,inmap,preidx,0,inorder.size()-1);
         }
     };
